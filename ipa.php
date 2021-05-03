61ebba085e94ef6e5ca0021d22665cb1598f6781
Thomas Beranek <github@beranek.one>
2021-05-03 20:12:09 +0200

    Remove file header and add comments.

--- ipa.php
+++ ipa.php
@@ -1,20 +1,16 @@
 <?php
-/*
-#-----------------------------------------
-| IP Anonymizer
-| https://github.com/beranek1/ip-anonymizer
-#-----------------------------------------
-| made by beranek1
-| https://github.com/beranek1
-#-----------------------------------------
-*/
 
 function anonymize_ip($ip) {
+    // Check type of ip address
     if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+        // Split IPv6 address in parts
         $ipparts = explode(":", $ip);
+        // Check whether address has maximum of 8 parts
         if(count($ipparts) == 8) {
+            // Only keep the first three parts
             $ip = $ipparts[0].":".$ipparts[1].":".$ipparts[2]."::";
         } else {
+            // Handle special cases
             if($ipparts[2] == "") {
                 $ip = $ipparts[0].":".$ipparts[1]."::";
             } else if($ipparts[1] == "") {
@@ -24,8 +20,11 @@ function anonymize_ip($ip) {
             }
         }
     } else if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+        // Split IPv4 address in parts
         $ipparts = explode(".", $ip);
+        // Check whether address consists of four parts
         if(count($ipparts) == 4) {
+            // Set the last part to 0
             $ip = $ipparts[0].".".$ipparts[1].".".$ipparts[2].".0";
         }
     }
